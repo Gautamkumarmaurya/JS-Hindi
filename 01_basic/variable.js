0a6f9778aa13c238fa6ddfdc2d85a1c17b261afe
Gautam Kumar <116151957+Gautamkumarmaurya@users.noreply.github.com>
2025-08-27 19:55:27 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,28 @@
+console.log("Gautam kumar");
+
+const accountId = 124678; // we cannot change the value of accountId
+
+let accountEmail = "gautam@gmail.com"
+
+var accountPassword = "gautam@123"; // we can change the value of accountPassword
+
+var accountPassword = "1234"
+let accountState;
+/*
+prefer not to use var
+because of issue in block scope and funtional scope
+*/
+
+accountCity = "Bangalore"
+
+console.log(accountId);
+console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountState);
+
+console.table([accountId, accountEmail , accountPassword , accountCity]);
+
+
+
+
+
